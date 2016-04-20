08c96e7b70de9f60ad2dd43804b9e8bf67a69d60
Tomas Novella <tomasnovella@gmail.com>
2016-04-20 12:35:50 +0200

    Missing Kitt API workarounds: own throttle implementation, missing manifest.content_scripts.css.

--- src/js/autofiller.js
+++ src/js/autofiller.js
@@ -5,9 +5,20 @@
 
 let $ = require('jquery');
 
+function hasEmailField() {
+  return true;
+}
+
+function setNeedsAutofillFlag(forms) {
+  forms.forEach((form) => {
+    if (hasEmailField(form)) {
+      form.needsAutofill = true;
+    }
+  });
+}
 function bindAutofilledValuesToForms(forms) {
   forms.forEach((form, key) => {
-    form.needsAutofill = key % 2 === 1;
+    form.needsAutofill = true; //key % 2 === 1;
     if (form.needsAutofill) {
       form.elements.each(function() {
         $(this).data("autofillCandidates", ["candidate1", "candidate2"]);
--- src/js/content.js
+++ src/js/content.js
@@ -1,52 +1,87 @@
 "use strict";
-
 let $ = require('jquery');
 require('jquery-ui');
 
 let _ = require('lodash');
-let autofiller = require('./autofiller');
+var autofiller = require('./autofiller');
+
+let stylesheetUrl = chrome.extension.getURL('./css/jquery-ui.css');
+$('head').append('<link rel="stylesheet" type="text/css" href="' + stylesheetUrl + '">');
+
+function throttle(func, timeout) {
+  // small bug inside:
+  var canFire = true;
+  var shouldFireOnWakeUp = false;
+  return function() {
+    if (canFire) {
+      func();
+      canFire = false;
+      setTimeout(function(){canFire = true; if(shouldFireOnWakeUp) {
+        func(); /* now I haven't set it right, so it will fire two times in a row */ }
+      }, timeout);
+    } else {
+      shouldFireOnWakeUp = true;
+    }
+  };
+}
 
 console.log('Content, chrome autofill!');
-//let elementsInFirstForm = $("form:nth(1)").find("input[type!='hidden'],textarea,select");
 
 // Extractor
 // extractedFORMS = [FORM]
 // FORM = {ACTION, ELEMENTS}
 // ELEMENTS = [JQUERY ELEMENT]
 function extractFormsFromCurrentPage() {
-  return $("form").toArray().map((HTMLForm) => {
+  let forms = $("form");
+  if (forms.length > 5) { // I'm on a page like facebook -> many forms
+    window.observer.disconnect();
+  }
+  return forms.toArray().map((HTMLForm) => {
     let jqForm = $(HTMLForm);
     let elements = jqForm.find("input[type!='hidden'],textarea,select");
-    return {
+    let parsedForm = {
       action: jqForm.attr("action"),
       elements: elements
     };
+    console.log("parsing form: ", parsedForm);
+    return parsedForm;
   });
 }
 
 function bindListenersToElementsOfForms(forms) {
   forms.forEach((form) => {
-    form.elements.each(function() {
+
+    form.elements.each(function () {
       $(this).autocomplete({
         source: $(this).data("autofillCandidates")
       });
+      this.onfocus = () => {
+        console.log("element", this, "was just focused, candidates:", $(this).data("autofillCandidates")); // $(this));
+      };
     });
   });
 }
 
-function main(processMutation) {
-  let observer = new MutationObserver(_.throttle(processMutation, 1000));
-
+function setUpObserver(processMutation) {
+  processMutation(); // sometimes, the page loads and no further mutations happen
+  let observer = new MutationObserver(throttle(processMutation, 1000));
+  //  let observer = new MutationObserver(_.throttle(processMutation, 1000));
+  window.observer = observer;
   observer.observe($('body')[0], {
     childList: true,
     subtree: true
   });
+
 }
 
-main(function(){
+setUpObserver(function () {
   let forms = extractFormsFromCurrentPage();
   bindListenersToElementsOfForms(forms);
   autofiller.bindAutofilledValuesToForms(forms);
 });
 
+// debug doesn't work in Kitt
 window.autofiller = autofiller;
+window.extractFormsFromCurrentPage = extractFormsFromCurrentPage;
+window.bindListenersToElementsOfForms = bindListenersToElementsOfForms;
+window.setUpObserver = setUpObserver;
